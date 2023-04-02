7ec369f76b3fe7651ee442b37bf4244096479d7e
Madjar Ivan <evanmadjar@gmail.com>
2023-04-02 10:33:23 +0300

    I made some changes on frontend

--- frontend/src/components/CreateRoute/CenterModal.jsx
+++ frontend/src/components/CreateRoute/CenterModal.jsx
@@ -12,6 +12,257 @@ import MySelect from "../MySelect"
 import ErrorIcon from "../../assets/images/ErrorIcon";
 
 
+const CenterModal = ({ handleClose, handleComplete, setRouteID }) => {
+  let { user } = useContext(AuthContext)
+  const navigate = useNavigate()
+
+  let [modalState, setModalState] = useState('route title')
+
+  const startNodes = [
+    {
+      id: 498,
+      title: 'кассир-бармен',
+    },
+    {
+      id: 218,
+      title: 'помощник бухгалтера',
+    },
+    {
+      id: 384,
+      title: 'официант',
+    },
+    {
+      id: 20,
+      title: 'МГТУ им. Н.Э. Баумана',
+    },
+    {
+      id: 31,
+      title: 'Высшая школа экономики',
+    }
+  ]
+  const endNodes = [
+    {
+      id: 204,
+      title: 'инженер',
+    },
+    {
+      id: 22,
+      title: 'менеджер',
+    },
+    {
+      id: 29,
+      title: 'начальник отдела',
+    },
+    {
+      id: 30,
+      title: 'начальник управления',
+    }
+  ]
+
+  let [profiles, setProfiles] = useState([])
+  let [routeTypes, setRouteTypes] = useState([])
+
+  let {authTokens} = useContext(AuthContext)
+
+  let [route, setRoute] = useState({
+    title: '',
+    user: user?.user_id,
+    start_node: '',
+    route_type: '',
+    target_node: ''
+  })
+
+  let [titleError, setTitleError] = useState('')
+  let [CVError, setCVError] = useState('')
+  let [routeTypeError, setRouteTypeError] = useState('')
+  let [targetError, setTargerError] = useState('')
+  let [isValid, setValid] = useState(true)
+
+  const handleData = () => {
+    setCVError('')
+    setRouteTypeError('')
+    setTargerError('')
+    isValid = true
+    setValid(isValid)
+    if (route.start_node == ''){
+      setCVError('Выберите старт')
+      isValid = false
+      setValid(isValid)
+    }
+    if (route.route_type == ''){
+      setRouteTypeError('Выберите трек')
+      isValid = false
+      setValid(isValid)
+    }
+    if (route.target_node == ''){
+      setTargerError('Выберите цель')
+      isValid = false
+      setValid(isValid)
+    }
+    if (isValid == true) {
+      if (route.route_type == 1){
+        APIService.getRouteBetween(route)
+          .then(result => {
+            if (result.message == 'OK'){
+              
+            }
+          })
+      }
+      else if (route.route_type == 2){
+        APIService.getRouteBy(route)
+          .then(result => console.log(result))
+      }
+      else if (route.route_type == 3){
+        APIService.getRouteTo(route)
+          .then(result => console.log(result))
+      }
+    }
+  }
+
+  useEffect(() => {
+    APIService.getCurrentProfiles(authTokens)
+    .then(result => {
+      setProfiles(result)
+      if (result.length == 0){
+        setModalState('no profiles')
+      }
+    })
+      
+    APIService.getRouteTypes()
+    .then(result => setRouteTypes(result))
+    // APIService.getTargets()
+    // .then(result => setTargets(result))
+  }, [])
+
+  let content;
+
+  if (modalState == 'no profiles'){
+    content = (
+      <NoProfilesBlock>
+        <CloseButton
+          src={image}
+          onClick={() => navigate('/my-routes')}
+        />
+        <BigLabel>
+          Не найдено ни одной анкеты!
+        </BigLabel>
+        <Image src={image4}/>
+        <ButtonWrapper>
+          <Button width="366px" height="46px" text="Заполнить анкету"
+            handler={() => navigate('/create-profile')}/>
+        </ButtonWrapper>
+      </NoProfilesBlock>
+    )
+  } else if (modalState == 'route title') {
+    content = (
+      <TitleBlock>
+        <CloseButton
+          src={image}
+          onClick={() => navigate('/my-routes')}
+        />
+        <BigLabel>
+          Как лодку назовешь, так она и поплывет
+        </BigLabel>
+        <Image src={image3}/>
+        <InputWrapper>
+          <SmallLabel>Название маршрута<Star>*</Star></SmallLabel>
+          <Input placeholder="Введите текст..."
+            value={route.title} onChange={(e) => setRoute({...route, title: e.target.value})}/>
+          {
+            titleError
+          ?
+            <ErrorMessage><ErrorIcon/>{ titleError }</ErrorMessage>
+          :
+            <></>
+          }
+        </InputWrapper>
+        <ButtonWrapper>
+          <Button width="366px" height="46px" text="Далее"
+            handler={() => {
+              setTitleError('')
+              if (route.title == ''){
+                setTitleError('Это обязательное поле')
+              }
+              else {
+                setModalState('select route')
+              }
+            }}/>
+        </ButtonWrapper>
+      </TitleBlock>
+    )
+  } else if (modalState == 'select route'){
+    content = (
+      <SelectBlock>
+        <CloseButton
+          src={image}
+          onClick={() => navigate('/my-routes')}
+        />
+        <BigLabel>
+          Выбор маршрута
+        </BigLabel>
+        <Image src={image3}/>
+        <SelectsWrapper>
+          <SmallLabel>Начало<Star>*</Star></SmallLabel>
+          <MySelect
+            listName="Выберите профессию/ место учебы"
+            items={startNodes}
+            selectedItem={route.start_node}
+            setSelectedItem={(item) => setRoute({...route, start_node: item?.id})}
+          />
+          {
+            CVError
+          ?
+            <ErrorMessage><ErrorIcon/>{ CVError }</ErrorMessage>
+          :
+            <></>
+          }
+          <SmallLabel>Карьерный трек<Star>*</Star></SmallLabel>
+          <MySelect
+            listName="Выберите трек"
+            items={routeTypes}
+            selectedItem={route.route_type}
+            setSelectedItem={(item) => setRoute({...route, route_type: item?.id})}
+          />
+          {
+            routeTypeError
+          ?
+            <ErrorMessage><ErrorIcon/>{ routeTypeError }</ErrorMessage>
+          :
+            <></>
+          }
+          <SmallLabel>Желаемая цель<Star>*</Star></SmallLabel>
+          <MySelect
+            listName="Выберите цель"
+            items={endNodes}
+            selectedItem={route.target_node}
+            setSelectedItem={(item) => setRoute({...route, target_node: item?.id})}
+          />
+          {
+            targetError
+          ?
+            <ErrorMessage><ErrorIcon/>{ targetError }</ErrorMessage>
+          :
+            <></>
+          }
+        </SelectsWrapper>
+        <Button width="452px" height="46px" text="Построить маршрут"
+          handler={() => {
+            handleData()
+          }}/>
+      </SelectBlock>
+    )
+  }
+
+  return (
+    <Background>
+      { content }
+    </Background>
+  )
+};
+
+export default CenterModal;
+
+
 const Background = styled.div`
   background-color: rgba(241, 241, 241, 0.8);
   width: 100vw;
@@ -132,202 +383,3 @@ const SelectsWrapper = styled.div`
   margin-top: -10px;
   margin-bottom: 50px;
 `
-
-
-const CenterModal = ({ handleClose, handleComplete, setRouteID }) => {
-  let { user } = useContext(AuthContext)
-  const navigate = useNavigate()
-
-  let [modalState, setModalState] = useState('route title')
-
-  let [profiles, setProfiles] = useState([])
-  let [routeTypes, setRouteTypes] = useState([])
-  let [targets, setTargets] = useState([])
-
-  let {authTokens} = useContext(AuthContext)
-
-  let [route, setRoute] = useState({
-    title: '',
-    user: user?.user_id,
-    CV: '',
-    route_type: '',
-    target: ''
-  })
-
-  let [titleError, setTitleError] = useState('')
-  let [CVError, setCVError] = useState('')
-  let [routeTypeError, setRouteTypeError] = useState('')
-  let [targetError, setTargerError] = useState('')
-  let [isValid, setValid] = useState(true)
-
-  const handleData = () => {
-    setCVError('')
-    setRouteTypeError('')
-    setTargerError('')
-    isValid = true
-    setValid(isValid)
-    if (route.CV == ''){
-      setCVError('Выберите анкету')
-      isValid = false
-      setValid(isValid)
-    }
-    if (route.route_type == ''){
-      setRouteTypeError('Выберите трек')
-      isValid = false
-      setValid(isValid)
-    }
-    if (route.target == ''){
-      setTargerError('Выберите цель')
-      isValid = false
-      setValid(isValid)
-    }
-    if (isValid == true) {
-      APIService.createRoute(route)
-      .then(result => setRouteID(result?.id))
-      handleClose()
-      handleComplete()
-    }
-  }
-
-  useEffect(() => {
-    APIService.getCurrentProfiles(authTokens)
-    .then(result => {
-      setProfiles(result)
-      if (result.length == 0){
-        setModalState('no profiles')
-      }
-    })
-      
-    APIService.getRouteTypes()
-    .then(result => setRouteTypes(result))
-    APIService.getTargets()
-    .then(result => setTargets(result))
-  }, [])
-
-  let content;
-
-  if (modalState == 'no profiles'){
-    content = (
-      <NoProfilesBlock>
-        <CloseButton
-          src={image}
-          onClick={() => navigate('/my-routes')}
-        />
-        <BigLabel>
-          Не найдено ни одной анкеты!
-        </BigLabel>
-        <Image src={image4}/>
-        <ButtonWrapper>
-          <Button width="366px" height="46px" text="Заполнить анкету"
-            handler={() => navigate('/create-profile')}/>
-        </ButtonWrapper>
-      </NoProfilesBlock>
-    )
-  } else if (modalState == 'route title') {
-    content = (
-      <TitleBlock>
-        <CloseButton
-          src={image}
-          onClick={() => navigate('/my-routes')}
-        />
-        <BigLabel>
-          Как лодку назовешь, так она и поплывет
-        </BigLabel>
-        <Image src={image3}/>
-        <InputWrapper>
-          <SmallLabel>Название маршрута<Star>*</Star></SmallLabel>
-          <Input placeholder="Введите текст..."
-            value={route.title} onChange={(e) => setRoute({...route, title: e.target.value})}/>
-          {
-            titleError
-          ?
-            <ErrorMessage><ErrorIcon/>{ titleError }</ErrorMessage>
-          :
-            <></>
-          }
-        </InputWrapper>
-        <ButtonWrapper>
-          <Button width="366px" height="46px" text="Далее"
-            handler={() => {
-              setTitleError('')
-              if (route.title == ''){
-                setTitleError('Это обязательное поле')
-              }
-              else {
-                setModalState('select route')
-              }
-            }}/>
-        </ButtonWrapper>
-      </TitleBlock>
-    )
-  } else if (modalState == 'select route'){
-    content = (
-      <SelectBlock>
-        <CloseButton
-          src={image}
-          onClick={() => navigate('/my-routes')}
-        />
-        <BigLabel>
-          Выбор маршрута
-        </BigLabel>
-        <Image src={image3}/>
-        <SelectsWrapper>
-          <SmallLabel>Анкета<Star>*</Star></SmallLabel>
-          <MySelect
-            listName="Выберите анкету"
-            items={profiles}
-            selectedItem={route.CV}
-            setSelectedItem={(item) => setRoute({...route, CV: item?.id})}
-          />
-          {
-            CVError
-          ?
-            <ErrorMessage><ErrorIcon/>{ CVError }</ErrorMessage>
-          :
-            <></>
-          }
-          <SmallLabel>Карьерный трек<Star>*</Star></SmallLabel>
-          <MySelect
-            listName="Выберите трек"
-            items={routeTypes}
-            selectedItem={route.route_type}
-            setSelectedItem={(item) => setRoute({...route, route_type: item?.id})}
-          />
-          {
-            routeTypeError
-          ?
-            <ErrorMessage><ErrorIcon/>{ routeTypeError }</ErrorMessage>
-          :
-            <></>
-          }
-          <SmallLabel>Желаемая цель<Star>*</Star></SmallLabel>
-          <MySelect
-            listName="Выберите цель"
-            items={targets}
-            selectedItem={route.target}
-            setSelectedItem={(item) => setRoute({...route, target: item?.id})}
-          />
-          {
-            targetError
-          ?
-            <ErrorMessage><ErrorIcon/>{ targetError }</ErrorMessage>
-          :
-            <></>
-          }
-        </SelectsWrapper>
-        <Button width="452px" height="46px" text="Построить маршрут"
-          handler={() => {
-            handleData()
-          }}/>
-      </SelectBlock>
-    )
-  }
-
-  return (
-    <Background>
-      { content }
-    </Background>
-  )
-};
-
-export default CenterModal;
